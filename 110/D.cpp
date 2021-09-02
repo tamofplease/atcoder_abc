f9334f4cc74e0b499f68e95d9d02350ca4212ad0
tamofplease <riddimdubstep@yahoo.co.jp>
2021-09-02 23:15:46 +0900

    ltns push

--- 110/D.cpp
+++ 110/D.cpp
@@ -1,58 +1,20 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
-vector<int> V;
 typedef long long ll;
-const int MOD = 1000000007 ;
 #define rep(i,n) for(int i=0;i<n;i++)
-int N;
-ll M;
-int a[1000000000] ={0};//iがa[i]乗
-int j = 2;
-int dp[100001][101];
-void Prime(int i = 2){
-    while(true){
-        
-        if(M%i!=0)break;
-        M/=i;
-        a[j]++;
-    }
-    j++;
-    if(M==1 )return;
-    if(M<i*i){
-        a[M]= 1;
-        return ;
-    }
-    Prime(i+1);
-}
-
+#define Rep(i,n) for(int i=1;i<=n;i++)
+const ll MOD = 1000000007;
+struct edge{
+    int to,weight;
+    edge(int to,int weight):to(to),weight(weight){}
+};
+using Graph = vector<vector<int>> ;
+using GraphEdge = vector<vector<int>> ;
+long long modinv(long long a, long long m=MOD) {long long b = m, u = 1, v = 0;while (b) {    long long t = a / b;    a -= t * b; swap(a, b);    u -= t * v; swap(u, v);}u %= m; if (u < 0) u += m;return u;}
+//MODの割り算の際にa/＝b;をa *= modinv(b,MOD);とする。
+int gcd(int a, int b){if (a%b == 0){return(b);}else{return(gcd(b, a%b));}}
+int lcm(int a, int b){return a * b / gcd(a, b);}
+ 
 int main(){
-    cin >> N >> M;
-    Prime();
-    int count=0;
-    rep(i,j){
-        rep(k,N+1)dp[k][0] = 1;
-        if(a[i]!=0){
-            for(int k=0;k<N;k++){
-                for(int l=1;l<=a[i];l++){
-                    if(l-1-a[i]>=0)dp[k+1][l] = (dp[k][l] + dp[k][l-1]-dp[k+1][l-1-a[i]]+MOD)%MOD;
-                    else{
-                        dp[k+1][l] = (dp[k][l] + dp[k+1][l-1])%MOD;
-                    }
-                }
-            }
-            V.push_back(dp[N][a[i]]);
-        }
-    }
-    ll ans = 1;
-    
-    while(!V.empty()){
-        ans *= V.back();
-        ans %= MOD;
-        V.pop_back();
-    }
-    cout << ans << endl;
     return 0;
-
 }
--- exp/A.cpp
+++ exp/A.cpp
@@ -12,8 +12,20 @@ using Graph = vector<vector<int>>;
 using GraphEdge = vector<vector<int>>;
 long long modinv(long long a, long long m=MOD) {long long b = m, u = 1, v = 0;while (b) {    long long t = a / b;    a -= t * b; swap(a, b);    u -= t * v; swap(u, v);}u %= m; if (u < 0) u += m;return u;}
 //MODの割り算の際にa/＝b;をa *= modinv(b,MOD);とする。
-int gcd(int a, int b){if (a%b == 0){return(b);}else{return(gcd(b, a%b));}}
+int gcd(int a, int b){if (a%b == 0){return b;}else{return gcd(b, a%b);}}
 int lcm(int a, int b){return a * b / gcd(a, b);}
+
+
+// returns (x,y,gcd(a,b))  s.t ax + by = gcd(a,b)
+
+tuple<int,int,int> ext_gcd(int a,int b){
+    if(a==0){
+        return make_tuple(0,1,b);
+    }else{
+        int X,Y,g;
+        tie[X,Y,g] = ext_gcd(b%a, a);
+    }
+}
 
 int main(){
 
