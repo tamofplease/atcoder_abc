9d4e75e53173dfaf4001402d67837c7d66895f54
tamofplease <riddimdubstep@yahoo.co.jp>
2022-01-22 16:17:31 +0900

    solve abc081

---
+++ abc081/d/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+typedef long long ll;
+#define rep(i,n) for(int i=0;i<n;i++)
+#define Rep(i,n) for(int i=1;i<=n;i++)
+#define ALL(v) v.begin(), v.end()
+const ll MOD = 1000000007;
+
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    int n; cin >> n;
+    vector<int> a(n);
+    rep(i, n) cin >> a[i];
+    return 0;
+}
