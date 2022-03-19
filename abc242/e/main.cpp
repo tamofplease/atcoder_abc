ff87fd85a40b66068bc18146962bd826be4e9165
miwanotomoki <tamofplease@miwanotomokinoMacBook-Pro.local>
2022-03-19 16:51:57 +0900

    create template of abc243

---
+++ abc242/e/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+typedef long long ll;
+#define rep(i,n) for(int i=0;i<n;i++)
+#define Rep(i,n) for(int i=1;i<=n;i++)
+#define ALL(v) v.begin(), v.end()
+const ll MOD = 1000000007;
+
+
+///  O(N*T) < 10^6
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    int t; cin >> t;
+    rep(i,t) {
+        int n; cin >> n;
+        string s; cin >> s;
+        rep(i,(n/2)) {
+            
+        }
+
+    }
+    return 0;
+}   
