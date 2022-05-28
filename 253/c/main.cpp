2f0d858aaf7ad76354a716df0057ab842a8fe186
miwanotomoki <mmiwatomoki@gmail.com>
2022-05-28 20:56:57 +0900

    create template

---
+++ 253/c/main.cpp
@@ -0,0 +1,16 @@
+// "Copyright 2022 tam"
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0 ; i < n ; i++ )
+#define Rep(i, n) for (int i = 1 ; i <= n ; i++ )
+
+#define ll int64_t
+
+int main() {
+    
+    return 0;
+}
