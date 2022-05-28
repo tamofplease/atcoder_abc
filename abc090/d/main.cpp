4223f57746b747bf42ed3ed81855bec92e5c5de3
miwanotomoki <mmiwatomoki@gmail.com>
2022-05-28 22:47:20 +0900

    create template

--- abc090/d/main.cpp
+++ abc090/d/main.cpp
@@ -1,31 +1,41 @@
+// "Copyright 2022 tam"
 #include <bits/stdc++.h>
-using namespace std;
 #include <atcoder/all>
-using namespace atcoder;
-typedef long long ll;
-#define rep(i,n) for(int i=0;i<n;i++)
-#define Rep(i,n) for(int i=1;i<=n;i++)
-#define ALL(v) v.begin(), v.end()
-const ll MOD = 1000000007;
 
-int main(){
+using namespace std;
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0 ; i < n ; i++ )
+#define Rep(i, n) for (int i = 1 ; i <= n ; i++ )
+
+#define ll int64_t
+
+int _main();
+
+int main() {
     cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    int n, k; cin >> n >> k;
-    // a % b >= k;
-    // <=> a / b = c ...k;
-    // a => b * c + k;
-    // a - b * c >= k
-    // a <= N
-    // k <= a <= N
-    
+    ios::sync_with_stdio(false);
+    _main();
+}
+
+int _main() {
+    cout << int('a') << " " << int('z') << endl;
+    cout << int('A') << " "  << int('Z') << endl;
+    int n, k;
+    cin >> n >> k;
     int ans = 0;
-    Rep(b, n) {
-        int tmp = 0; 
-        while(tmp <= n-k) {
-            tmp += b;
-            ans++;
+    // a < b
+    for (int i = k ; i <= n ; i++) {
+        ans += min(n-i, n-k);
+    }
+
+    /// a > b
+    for (int a = 1 ; a <= n ; a++) {
+        for (int b = k + 1 ; b <= min(a-1, a-k) ; b++) {
+            if (a % b >= k) ans++;
         }
     }
+    cout << ans << endl;
+
     return 0;
 }
