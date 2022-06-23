5ceef6784b29cc1c1959fc26173f94be18ce517e
miwanotomoki <mmiwatomoki@gmail.com>
2022-06-23 11:28:02 +0900

    solve abc256 with virtually

---
+++ abc256/e/main.cpp
@@ -0,0 +1,23 @@
+// "Copyright 2022 tam"
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define Rep(i, n) for (int i = 1; i <= n; i++)
+
+#define ll int64_t
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> x(n), c[n];
+    for (auto &ref : x)
+        cin >> ref;
+    for (auto &ref : c)
+        cin >> ref;
+
+    return 0;
+}
