179eed648d5eff3e683d283e08de29091f2d2a58
miwanotomoki <mmiwatomoki@gmail.com>
2022-07-08 10:22:58 +0900

    solve abc258 of E prob

--- abc258/e/main.cpp
+++ abc258/e/main.cpp
@@ -15,9 +15,54 @@ int main() {
     int n, q, x;
     cin >> n >> q >> x;
     vector<int> w(n);
-    rep(i, w) cin >> w[i];
-    vector<ll> sums(n + 1, 0);
-    Rep(i, n) { sums[i] += sums[i - 1] + w[i - 1] }
+    ll allW = 0;
+    rep(i, n) {
+        cin >> w[i];
+        allW += w[i];
+    }
+    vector<ll> k(q);
+    rep(i, q) cin >> k[i];
 
+    vector<ll> nums(n, 0);
+    ll front = 0;
+    ll cur = 0;
+    for (int back = 0; back < n; back++) {
+        if (x - cur > allW) {
+            front += ((x - cur) / allW) * n;
+            cur += ((x - cur) / allW) * allW;
+        }
+        while (cur < x) {
+            cur += w[front % n];
+            front++;
+        }
+        nums[back] = front - back;
+        cur -= w[back];
+    }
+    int oneLoop = 0;
+    vector<int> next(n);
+    vector<int> been(n, -1);
+    vector<int> ma;
+    int iter = 0;
+    while (been[iter] == -1) {
+        been[iter] = oneLoop;
+        ma.push_back(iter);
+        next[iter] = (iter + nums[iter]) % n;
+        iter += nums[iter] % n;
+        iter %= n;
+        oneLoop++;
+    }
+    oneLoop -= been[iter];
+
+    for (ll tgt : k) {
+        tgt--;
+        if (been[iter] >= tgt) {
+            cout << nums[ma[tgt]] << endl;
+            continue;
+        }
+        tgt -= been[iter];
+        tgt %= oneLoop;
+        tgt += been[iter];
+        cout << nums[ma[tgt]] << endl;
+    }
     return 0;
 }
