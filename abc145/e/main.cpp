310b844c6ec23156d6c0d9a36b550daf77ad59e6
tamofplease <riddimdubstep@yahoo.co.jp>
2022-07-30 00:05:59 +0900

    solve abc145

--- abc145/e/main.cpp
+++ abc145/e/main.cpp
@@ -9,6 +9,7 @@ using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < n; i++)
 #define Rep(i, n) for (int i = 1; i <= n; i++)
 #define ALL(v) v.begin(), v.end()
+#define chmax(x, y) x = max(x, y)
 
 #define ll int64_t
 
@@ -16,21 +17,25 @@ using P = pair<int, int>;
 
 const int MOD = 1000000007;
 
-bool comp(const P& a, const P& b) { return a.first + a.second < b.first + b.second; }
-
 int main() {
     int n, t;
     cin >> n >> t;
     vector<P> v(n);
     rep(i, n) { cin >> v[i].first >> v[i].second; }
-    sort(ALL(v), comp);
-    vector<vector<int>> dp(n + 1, vector<int>(3000 * 2 + 1, 0));
-    rep(i, n) {
-        rep(j, t) { dp[i + 1][j + v[i].first] = max(dp[i + 1][j + v[i].first], dp[i][j] + v[i].second); }
+    sort(ALL(v));
+    vector<vector<int>> dp(n, vector<int>(t, 0));
+    // select n - 1 and eq or less than t - 1
+    rep(i, n - 1) {
+        rep(j, t) {
+            dp[i + 1][j] = dp[i][j];
+            if (j - v[i].first >= 0) {
+                chmax(dp[i + 1][j], dp[i][j - v[i].first] + v[i].second);
+            }
+        }
     }
     int ans = 0;
-    rep(i, n + 1) {
-        rep(j, 3000 * 2 + 1) { ans = max(ans, dp[i][j]); }
+    rep(i, n) {
+        rep(j, t) { ans = max(ans, dp[i][j] + v[i].second); }
     }
     cout << ans << endl;
 
