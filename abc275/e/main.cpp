a76fe3095c99c8129cd9ff340ba43d48f7852305
tamofplease <riddimdubstep@yahoo.co.jp>
2022-11-23 14:35:07 +0900

    solve abc275 with cpp

--- abc275/e/main.cpp
+++ abc275/e/main.cpp
@@ -1,7 +1,11 @@
 // "Copyright 2022 tam"
 #include <bits/stdc++.h>
 
+#include <atcoder/modint>
+
 using namespace std;
+
+using mint = atcoder::modint998244353;
 
 #define rep(i, n) for (int i = 0; i < n; i++)
 #define Rep(i, n) for (int i = 1; i <= n; i++)
@@ -18,5 +22,23 @@ const int MOD = 1000000007;
 int main() {
     int n, m, k;
     cin >> n >> m >> k;
+    vector<vector<mint>> dp(k + 1, vector(n + 1, mint(0)));
+    mint m_inv = mint(1) / m;
+    dp[0][0] = 1;
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (j == n) {
+                dp[i + 1][j] += dp[i][j];
+                continue;
+            }
+            for (int l = 1; l <= m; l++) {
+                int nx = j + l;
+                if (nx > n) nx = n - (nx - n);
+                dp[i + 1][nx] += dp[i][j] * m_inv;
+            }
+        }
+    }
+    cout << dp.back().back().val() << endl;
+
     return 0;
 }
