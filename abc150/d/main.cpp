24b9d0e4fc46fbb99e7ad99cd2469a46da655878
tamofplease <riddimdubstep@yahoo.co.jp>
2022-11-29 01:21:11 +0900

    solve d prob of abc150

--- abc150/d/main.cpp
+++ abc150/d/main.cpp
@@ -1,14 +1,13 @@
 // "Copyright 2022 tam"
 #include <bits/stdc++.h>
 
-#include <atcoder/all>
-
 using namespace std;
-using namespace atcoder;
 
 #define rep(i, n) for (int i = 0; i < n; i++)
 #define Rep(i, n) for (int i = 1; i <= n; i++)
 #define ALL(v) v.begin(), v.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
 
 #define ll int64_t
 
@@ -17,10 +16,23 @@ using P = pair<int, int>;
 const int MOD = 1000000007;
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n);
-    rep(i, n) { cin >> a[i]; }
-
+    ll n, m, lc;
+    cin >> n >> m >> lc, lc /= 2;
+    vector<int> aa(n);
+    aa[0] = lc;
+    bool valid = true;
+    Rep(i, n - 1) {
+        cin >> aa[i], aa[i] /= 2;
+        lc = lcm(lc, aa[i]);
+        if (lc > m) {
+            valid = false;
+            break;
+        }
+    }
+    if (valid && all_of(ALL(aa), [lc](const int a) -> bool { return (lc / a) % 2; })) {
+        cout << ((m / lc) - 1) / 2 + 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
     return 0;
 }
