d3c7b3df940e68f72efc6f4f280ea0fed9e2e8e1
tamofplease <riddimdubstep@yahoo.co.jp>
2022-12-20 01:05:49 +0900

    solve abc237 of e prob

--- abc237/e/main.cpp
+++ abc237/e/main.cpp
@@ -1,66 +1,57 @@
 // "Copyright 2022 tam"
 #include <bits/stdc++.h>
-#include <atcoder/all>
 
 using namespace std;
-using namespace atcoder;
 
-#define rep(i, n) for (int i = 0 ; i < n ; i++)
-#define Rep(i, n) for (int i = 1 ; i <= n ; i++)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define Rep(i, n) for (int i = 1; i <= n; i++)
 #define ALL(v) v.begin(), v.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
 
 #define ll int64_t
 
-struct Edge {
-    ll cost;
-    int to;
-    Edge(ll cost, int to) : cost(cost), to(to) {}
-};
+using P = pair<ll, int>;
 
-using P = pair<ll, ll>;
+const int MOD = 1000000007;
 
 int main() {
     int n, m;
     cin >> n >> m;
     vector<ll> h(n);
-    for (auto& ref : h) cin >> ref;
-    vector<vector<Edge>> g(n);
+    rep(i, n) { cin >> h[i]; }
+    vector<vector<int>> to(n);
+    map<pair<int, int>, ll> costs;
     rep(i, m) {
         int u, v;
         cin >> u >> v, u--, v--;
-        if (h[u] < h[v]) {
-            g[u].emplace_back(h[v] - h[u], u);
-            g[v].emplace_back(0, v);
-            continue;
-        }
-        g[v].emplace_back(h[u] - h[v], v);
-        g[u].emplace_back(0, u);
+        to[u].push_back(v);
+        to[v].push_back(u);
+        costs[{u, v}] = (h[v] - h[u]) * ((h[u] < h[v]) ? 1 : 0);
+        costs[{v, u}] = (h[u] - h[v]) * ((h[v] < h[u]) ? 1 : 0);
     }
-    priority_queue<P, vector<P>, greater<P>> q;
-    vector<bool> been(n, false);
-    vector<ll> costs(n, INT64_MAX);
-    q.emplace(0, 0);
-    while (!q.empty()) {
-        auto [_, cur] = q.top();
-        q.pop();
-        been[cur] = true;
-        for (auto [nxt, cost] : g[cur]) {
-            if (been[nxt]) {
-                continue;
+
+    vector<ll> fixed(n, INT64_MAX), floating(n, INT64_MAX);
+    floating[0] = 0;
+    priority_queue<P, vector<P>, greater<P>> pq;
+    pq.emplace(0, 0);
+    ll ans = INT64_MIN;
+    while (!pq.empty()) {
+        auto [cost, cur] = pq.top();
+        pq.pop();
+        if (fixed[cur] != INT64_MAX) continue;
+        fixed[cur] = cost;
+        ans = max(h[0] - fixed[cur] - h[cur], ans);
+        for (const int nxt : to[cur]) {
+            if (fixed[nxt] != INT64_MAX) continue;
+            if (cost + costs[{cur, nxt}] < floating[nxt]) {
+                floating[nxt] = cost + costs[{cur, nxt}];
+                pq.emplace(floating[nxt], nxt);
             }
-            if (costs[nxt] > costs[cur] + cost) {
-                costs[nxt] = costs[cur] + cost;
-                q.emplace(costs[nxt], nxt);
-            }
         }
     }
-    ll ans = INT64_MAX;
-    Rep(i, n - 1) {
-        ans = min(ans, h[i] + costs[i]);
-    }
+
     cout << ans << endl;
-
-
 
     return 0;
 }
