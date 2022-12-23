a9319fe10f97cb9ebde8a87f6db66025137b8396
tamofplease <riddimdubstep@yahoo.co.jp>
2022-12-24 02:17:19 +0900

    solve abc197 with cpp

--- abc197/d/main.cpp
+++ abc197/d/main.cpp
@@ -1,20 +1,36 @@
+// "Copyright 2022 tam"
 #include <bits/stdc++.h>
-#include <atcoder/all>
 
 using namespace std;
-using namespace atcoder;
 
-#define rep(i,n) for(int i=0;i<n;i++)
-#define Rep(i,n) for(int i=1;i<=n;i++)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define Rep(i, n) for (int i = 1; i <= n; i++)
+#define ALL(v) v.begin(), v.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
 
-#define ll long long
+#define ll int64_t
 
-#define P pair<int,int>
+using P = pair<int, int>;
+
+const int MOD = 1000000007;
+
+using C = complex<double>;
+
+C inC() {
+    double x, y;
+    cin >> x >> y;
+    return C(x, y);
+}
 
 int main() {
-  int n; cin >> n;
-  P p0, opp, center;
-  cin >> p0.first >> p0.second >> opp.first >> opp.second;
-  
-  return 0;
+    int n;
+    cin >> n;
+    C s = inC(), g = inC();
+    C o = (s + g) / 2.0;
+    double PI = acos(-1);
+    double rad = PI * 2 / (double)n;
+    C r(cos(rad), sin(rad));
+    C ans = o + (s - o) * r;
+    printf("%.11f %.11f\n", ans.real(), ans.imag());
 }
