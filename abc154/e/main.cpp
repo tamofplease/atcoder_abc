32b31a8629323295da11c90c088e0f52c4c3d889
tamofplease <riddimdubstep@yahoo.co.jp>
2022-12-24 09:50:47 +0900

    solve abc154 with cpp

--- abc154/e/main.cpp
+++ abc154/e/main.cpp
@@ -1,10 +1,7 @@
 // "Copyright 2022 tam"
 #include <bits/stdc++.h>
 
-#include <atcoder/all>
-
 using namespace std;
-using namespace atcoder;
 
 #define rep(i, n) for (int i = 0; i < n; i++)
 #define Rep(i, n) for (int i = 1; i <= n; i++)
@@ -18,21 +15,41 @@ using P = pair<int, int>;
 
 const int MOD = 1000000007;
 
+ll C(ll a, ll b) {
+    if (b == 1) return a;
+    if (b == 2) return a * (a - 1) / 2;
+    return 1;
+}
+
 int main() {
-    string n;
+    string s;
+    cin >> s;
+    const int n = s.length();
     int k;
-    cin >> n >> k;
-    map<pair<string, int>, int> memo;
-    // a = 10m * r;
-    auto recur = [&](auto f, string N, int K) -> int {
-        if (memo.find({N, K}) != memo.end()) return memo[{N, K}];
-        if (K == 0) {
-            return 0;
-        }
-        string nxt = N.substr(0, N.length() - 1);
-        memo[{nxt, K - 1}] = f(nxt, K - 1);
-        nxt memo[{}](N[N.length() - 1] - '0') * memo[{nxt, K - 1}];
-    };
+    cin >> k;
+
+    if (n < k) {
+        cout << 0 << endl;
+        return 0;
+    }
+    ll ans = 0;
+    ll base = 1;
+    rep(i, k) base *= 9;
+    for (int i = k; i < n; i++) ans += base * C(i - 1, k - 1);
+    int tmp_k = k;
+
+    for (int i = 0; i < n; i++) {
+        int a = s[i] - '0';
+        if (a == 0) continue;
+        int smaller = a - 1;
+        if (i != 0) ans += C(n - i - 1, tmp_k) * base;
+        base /= 9;
+        ans += smaller * C(n - i - 1, tmp_k - 1) * base;
+        if (--tmp_k == 0) break;
+    }
+    int cnt = accumulate(s.begin(), s.end(), 0, [](int prev, char cur) -> int { return prev + (cur != '0'); });
+    if (cnt >= k) ans++;
+    cout << ans << endl;
 
     return 0;
 }
