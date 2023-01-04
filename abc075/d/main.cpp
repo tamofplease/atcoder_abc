8332831e58eb1bf4e96d10e15b70f3a0a73e6cd2
tamofplease <riddimdubstep@yahoo.co.jp>
2023-01-05 00:37:46 +0900

    solve d prob of abc075

--- abc075/d/main.cpp
+++ abc075/d/main.cpp
@@ -1,43 +1,58 @@
+// "Copyright 2022 tam"
 #include <bits/stdc++.h>
-using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
-typedef long long ll;
-#define rep(i,n) for(int i=0;i<n;i++)
-#define Rep(i,n) for(int i=1;i<=n;i++)
-#define ALL(v) v.begin(), v.end()
-const ll MOD = 1000000007;
 
-int main(){
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    int n, k; cin >> n >> k;
-    vector<pair<ll, ll>> p(n);
-    rep(i, n) cin >> p[i].first >> p[i].second;
-    sort(ALL(p));
-    ll ans = INT64_MAX;
-    rep(i, n-k+1) {
-        ll mi = INT64_MAX, ma = INT64_MIN;
-        for(int j=i;j<i+k;j++) {
-            mi = min(p[j].second, mi);
-            ma = max(p[j].second, ma);
-        }
-        ll tmp = abs(ma - mi) * abs(p[i+k-1].first - p[i].first);
-        ans = min(tmp, ans);
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define Rep(i, n) for (int i = 1; i <= n; i++)
+#define ALL(v) v.begin(), v.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+
+#define ll int64_t
+
+using P = pair<int, int>;
+
+const int MOD = 1000000007;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<vector<ll>> v;
+    rep(i, n) {
+        int x, y;
+        cin >> x >> y;
+        v.push_back({x, y});
     }
-    rep(i, n) swap(p[i].second, p[i].first);
-    sort(ALL(p));
-    rep(i, n - k + 1) {
-        ll mi = INT64_MAX, ma = INT64_MIN;
-        for (int j = i; j < i + k; j++) {
-            mi = min(p[j].second, mi);
-            ma = max(p[j].second, ma);
+    ll ans = INT64_MAX;
+    vector<vector<ll>> vertical(n, vector<ll>(n, 0)), horizontal(n, vector<ll>(n, 0));
+    rep(r, n) rep(l, n) {
+        ll right = v[r][0], left = v[l][0];
+        rep(i, n) {
+            if (left <= v[i][0] && v[i][0] <= right) {
+                horizontal[l][r] |= (1LL << i);
+            }
         }
-        ll tmp = abs(ma - mi) * abs(p[i + k - 1].first - p[i].first);
-        ans = min(tmp, ans);
+    }
+    rep(t, n) rep(b, n) {
+        ll top = v[t][1], bottom = v[b][1];
+        rep(i, n) {
+            if (bottom <= v[i][1] && v[i][1] <= top) {
+                vertical[b][t] |= (1LL << i);
+            }
+        }
+    }
+
+    rep(r, n) rep(l, n) {
+        ll hor = horizontal[l][r];
+        rep(t, n) rep(b, n) {
+            ll ver = vertical[b][t];
+            if (__builtin_popcountll(ver & hor) >= k) {
+                ans = min(ans, (v[r][0] - v[l][0]) * (v[t][1] - v[b][1]));
+            }
+        }
     }
     cout << ans << endl;
 
-    
     return 0;
 }
