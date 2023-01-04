0fc19b51158406ebee18d5a2b6638ab68dab43bc
tamofplease <riddimdubstep@yahoo.co.jp>
2023-01-04 15:21:42 +0900

    solve d prob of abc087

--- abc087/d/main.cpp
+++ abc087/d/main.cpp
@@ -1,45 +1,93 @@
+// "Copyright 2022 tam"
 #include <bits/stdc++.h>
-using namespace std;
+
 #include <atcoder/all>
+
+using namespace std;
 using namespace atcoder;
-typedef long long ll;
-#define rep(i,n) for(int i=0;i<n;i++)
-#define Rep(i,n) for(int i=1;i<=n;i++)
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define Rep(i, n) for (int i = 1; i <= n; i++)
+#define rRep(i, n) for (int i = n; i >= 1; i--)
 #define ALL(v) v.begin(), v.end()
-const ll MOD = 1000000007;
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
 
-int main(){
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    int n, m; cin >> n >> m;
-    vector<int> l(m), r(m), d(m);
-    vector<bool> visited(n, false);
-    rep(i, m) {
-        cin >> l[i] >> r[i] >> d[i];
-        l[i]--; r[i]--;
-    }
-    queue<int> q;
-    q.push
-    map<int, int> ma;
-    rep(i, m) {
-        ma[r[i]] = ma[l[i]] + d[i];
+#define ll int64_t
 
+using P = pair<int, int>;
+// using mint = modint1000000007;
+using mint = modint998244353;
+
+const int MOD = 1000000007;
+
+template <typename T, int FAC_MAX>
+struct Comb {
+   private:
+    vector<T> fac, ifac;
+
+   public:
+    Comb() {
+        fac.resize(FAC_MAX, 1);
+        ifac.resize(FAC_MAX, 1);
+        Rep(i, FAC_MAX - 1) fac[i] = fac[i - 1] * i;
+        ifac[FAC_MAX - 1] = T(1) / fac[FAC_MAX - 1];
+        rRep(i, FAC_MAX - 2) ifac[i] = ifac[i + 1] * T(i + 1);
     }
-    for(int i=m-1;i>=0;i--) {
-        ma[l[i]] = ma[r[i]] - d[i];
+    T aPb(int a, int b) {
+        if (b < 0 || a < b) return T(0);
+        return fac[a] * ifac[a - b];
     }
+    T aCb(int a, int b) {
+        if (b < 0 || a < b) return T(0);
+        return fac[a] * ifac[a - b] * ifac[b];
+    }
+    T nHk(int n, int k) {
+        if (n == 0 && k == 0) return T(1);
+        if (n <= 0 || k < 0) return 0;
+        return aCb(n + k - 1, k);
+    }
+    T pairCombination(int n) {
+        if (n % 2 == 1) return T(0);
+        return fac[n] * ifac[n / 2] / (T(2) ^ (n / 2));
+    }
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pair<int, int>>> right(n);
+    dsu ds(n);
     rep(i, m) {
-        if(ma[r[i]] != ma[l[i]] + d[i]) {
-            puts("No");
-            return 0;
-        }
+        int l, r, d;
+        cin >> l >> r >> d, l--, r--;
+        ds.merge(l, r);
+        right[l].push_back({r, d});
+        right[r].push_back({l, -d});
     }
-    for(int i=m-1;i>=0;i--) {
-        if(ma[l[i]] != ma[r[i]] - d[i]) {
-            puts("No");
-            return 0;
+    bool ans = true;
+    vector<ll> position(n, INT64_MIN);
+    auto dfs = [&](auto f, int cur) -> void {
+        for (const auto [nxt, dist] : right[cur]) {
+            if (position[nxt] == INT64_MIN) {
+                position[nxt] = position[cur] + dist;
+                f(f, nxt);
+                continue;
+            }
+            if (position[nxt] != position[cur] + dist) {
+                ans = false;
+                break;
+            }
         }
+    };
+    for (vector<int>& d : ds.groups()) {
+        position[d[0]] = 0;
+        dfs(dfs, d[0]);
     }
-    puts("Yes");
+    if (ans)
+        puts("Yes");
+    else
+        puts("No");
     return 0;
 }
