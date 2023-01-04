64bbe6ae06933376b1e53d13b8c4776cb95bf9bb
tamofplease <riddimdubstep@yahoo.co.jp>
2023-01-04 20:02:57 +0900

    solve abc117 with cpp

--- abc117/d/main.cpp
+++ abc117/d/main.cpp
@@ -15,4 +15,39 @@ using P = pair<int, int>;
 
 const int MOD = 1000000007;
 
-int main() {}
+int main() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll& ref : a) {
+        cin >> ref;
+    }
+    vector<ll> ones, zeros;
+    for (ll i = 0; i < 42; i++) {
+        int one = 0, zero = 0;
+        for (ll aa : a) (((aa >> i) & 1LL) ? one : zero)++;
+        ones.push_back(one);
+        zeros.push_back(zero);
+    }
+
+    // dp[i][0] -> same, dp[i][1] -> smaller
+    ll same = 0, smaller = 0;
+    for (ll i = 40; i >= 0; i--) {
+        ll one_gain = (1LL << i) * zeros[i], zero_gain = (1LL << i) * ones[i];
+        ll nxt_same = 0, nxt_smaller = 0;
+        if (k & (1LL << i)) {
+            nxt_same = same + one_gain;
+            if (smaller != 0) {
+                nxt_smaller = max(smaller + max(zero_gain, one_gain), same + zero_gain);
+            } else {
+                nxt_smaller = max(same, smaller + zero_gain);
+            }
+        } else {
+            nxt_same = same + zero_gain;
+            if (smaller != 0) nxt_smaller = smaller + max(zero_gain, one_gain);
+        }
+        smaller = nxt_smaller, same = nxt_same;
+    }
+    cout << max(smaller, same) << endl;
+    return 0;
+}
