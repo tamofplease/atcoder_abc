a7ad05a4a046f4a86483c4ecf185dde2dff6f7eb
tamofplease <riddimdubstep@yahoo.co.jp>
2023-01-04 15:55:00 +0900

    solve c prob of abc074

--- abc074/c/main.cpp
+++ abc074/c/main.cpp
@@ -1,61 +1,40 @@
+// "Copyright 2022 tam"
 #include <bits/stdc++.h>
+
 using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
-typedef long long ll;
-#define rep(i,n) for(int i=0;i<n;i++)
-#define Rep(i,n) for(int i=1;i<=n;i++)
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define Rep(i, n) for (int i = 1; i <= n; i++)
 #define ALL(v) v.begin(), v.end()
-const ll MOD = 1000000007;
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
 
+#define ll int64_t
 
-// dp[i] 水i*100gにとける砂糖の最大量
-int main(){
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    int a,b,c,d,e,f; cin >> a >> b >> c >> d >> e >>f;
-    f/=100;
-    vector<int> dp(f+1, -1);
-    dp[0] = 0;
-    Rep(i, f) {
-        if(dp[i-a] == 0) dp[i] = 0;
-        if(dp[i-b] == 0) dp[i] = 0;
-    }
-    Rep(i, f) cout << dp[i] << endl;
-    Rep(i, f) {
-        if(dp[i-c] >= 0) {
-            while(true) {
-                if(dp[i] == -1 && c <= i*e) {
-                    dp[i] = c;
-                }
-                else if(dp[i]+c <= i*e) {
-                    dp[i] += c;
-                } else {
-                    break;
-                }
-            }
+using P = pair<int, int>;
+
+const int MOD = 1000000007;
+
+int main() {
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    int ans_sugar = 0, ans_sugar_water = 0;
+    set<pair<int, int>> been;
+    auto dfs1 = [&](auto dfs, int water, int sugar) -> void {
+        int sugar_water = water + sugar;
+        if (sugar_water > f || been.count({water, sugar})) return;
+        if (e * sugar_water >= sugar * (100 + e) && (ans_sugar * sugar_water) <= (sugar * ans_sugar_water)) {
+            ans_sugar = sugar;
+            ans_sugar_water = sugar_water;
         }
-        if(dp[i-d] >= 0) {
-            while(true) {
-                if( dp[i] == -1 && d <= i*e) {
-                    dp[i] = d;
-                } else if(dp[i] + d <= i*e) {
-                    dp[i] += d;
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-    Rep(i, f) cout << dp[i] << endl;
-    int ma = 0;
-    int sug = 0, wat = 0;
-    Rep(i, f) {
-        if(i*100+dp[i] <= f && ma < 100*dp[i]/(100*i+dp[i])) {
-            ma = 100 * dp[i] / (100 * i + dp[i]);
-            sug = dp[i], wat = i * 100 + dp[i];
-        }
-    }
-    cout << wat << " " << sug << endl;
+        been.insert({water, sugar});
+        dfs(dfs, water + 100 * a, sugar);
+        dfs(dfs, water + 100 * b, sugar);
+        dfs(dfs, water, sugar + c);
+        dfs(dfs, water, sugar + d);
+    };
+    dfs1(dfs1, 0, 0);
+    cout << ans_sugar_water << " " << ans_sugar << endl;
+
     return 0;
 }
