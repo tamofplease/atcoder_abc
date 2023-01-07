0730582cb52145e491b94145952a143141f12423
tamofplease <riddimdubstep@yahoo.co.jp>
2023-01-07 16:42:45 +0900

    solve f prob of abc148

--- abc148/f/main.cpp
+++ abc148/f/main.cpp
@@ -1,10 +1,7 @@
 // "Copyright 2022 tam"
 #include <bits/stdc++.h>
 
-#include <atcoder/all>
-
 using namespace std;
-using namespace atcoder;
 
 #define rep(i, n) for (int i = 0; i < n; i++)
 #define Rep(i, n) for (int i = 1; i <= n; i++)
@@ -18,4 +15,38 @@ using P = pair<int, int>;
 
 const int MOD = 1000000007;
 
-int main() { return 0; }
+int main() {
+    int n, u, v;
+    cin >> n >> u >> v, u--, v--;
+    vector<vector<int>> to(n);
+    vector<int> dist_t(n, INT32_MAX), dist_a(n, INT32_MAX);
+    rep(i, n - 1) {
+        int a, b;
+        cin >> a >> b, a--, b--;
+        to[a].push_back(b);
+        to[b].push_back(a);
+    }
+    auto dfs = [&](auto f, vector<int>& dist, int cur, int prev) -> void {
+        dist[cur] = dist[prev] + 1;
+        for (int nxt : to[cur]) {
+            if (dist[nxt] != INT32_MAX) continue;
+            f(f, dist, nxt, cur);
+        }
+    };
+    dist_t[u] = 0;
+    dist_a[v] = 0;
+    for (int nxt : to[u]) {
+        dfs(dfs, dist_t, nxt, u);
+    }
+    for (int nxt : to[v]) {
+        dfs(dfs, dist_a, nxt, v);
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (dist_a[i] > dist_t[i]) {
+            ans = max(ans, dist_a[i] - 1);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
