05c88eddd9f3859b2b6b7bdac615ac12d249fd00
tamofplease <riddimdubstep@yahoo.co.jp>
2023-01-08 22:18:24 +0900

    solve f prob of abc174

--- abc174/f/main.cpp
+++ abc174/f/main.cpp
@@ -1,28 +1,49 @@
 // "Copyright 2022 tam"
 #include <bits/stdc++.h>
 
+#include <atcoder/fenwicktree>
+
 using namespace std;
 
 #define rep(i, n) for (int i = 0; i < n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
 #define Rep(i, n) for (int i = 1; i <= n; i++)
+#define rRep(i, n) for (int i = n; i >= 1; i--)
 #define ALL(v) v.begin(), v.end()
 #define chmax(x, y) x = max(x, y)
 #define chmin(x, y) x = min(x, y)
 
-#define ll int64_t
-
-using P = pair<int, int>;
-
-const int MOD = 1000000007;
-
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n, q;
     cin >> n >> q;
-    vector<int> c(n);
-    rep(i, n) cin >> c[i];
+    vector<int> c(n), ans(q);
+    for (int& ref : c) cin >> ref;
+    vector<int> ma(500001, -1);
+    vector<vector<int>> range_and_query;
+    map<vector<int>, vector<int>> m;
     rep(i, q) {
         int l, r;
-        cin >> l >> r;
+        cin >> l >> r, l--;
+        range_and_query.push_back({l, 0, r});
+        m[{l, 0, r}].push_back(i);
     }
+    rep(i, n) {
+        if (ma[c[i]] != -1) range_and_query.push_back({ma[c[i]], 1, i});
+        ma[c[i]] = i;
+    }
+    sort(ALL(range_and_query), greater<vector<int>>());
+    atcoder::fenwick_tree<int> fw(n);
+    for (const vector<int>& v : range_and_query) {
+        if (v[1]) {
+            fw.add(v[2], 1);
+            continue;
+        }
+        int a = v[2] - v[0] - fw.sum(0, v[2]);
+        for (const int& mm : m[v]) ans[mm] = a;
+    }
+    for (const int& aa : ans) cout << aa << endl;
+
     return 0;
 }
