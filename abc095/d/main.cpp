8fe1cb80ce6becb7a9980296f300549fdcba36bb
tamofplease <riddimdubstep@yahoo.co.jp>
2023-01-21 18:47:57 +0900

    update template of abc095

--- abc095/d/main.cpp
+++ abc095/d/main.cpp
@@ -1,17 +1,26 @@
+// "Copyright 2022 tam"
 #include <bits/stdc++.h>
-using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
-typedef long long ll;
-#define rep(i,n) for(int i=0;i<n;i++)
-#define Rep(i,n) for(int i=1;i<=n;i++)
-#define ALL(v) v.begin(), v.end()
-const ll MOD = 1000000007;
 
-int main(){
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    int n, c; cin >> n >> c;
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define Rep(i, n) for (int i = 1; i <= n; i++)
+#define ALL(v) v.begin(), v.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+
+#define ll int64_t
+
+using P = pair<int, int>;
+
+const int MOD = 1000000007;
+
+int main() {
+    ll n, c;
+    cin >> n >> c;
+    vector<ll> x, v;
     rep(i, n) cin >> x[i] >> v[i];
+    ll ans = 0;
+    ll cur = 0;
     return 0;
 }
