b085f504db39eef31589a8fd3bc7cadfad991346
tamofplease <riddimdubstep@yahoo.co.jp>
2023-08-10 00:00:33 +0900

    solve a prob of abc299

---
+++ abc299/a/main.cpp
@@ -0,0 +1,37 @@
+// "Copyright 2022 tam"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define Rep(i, n) for (int i = 1; i <= n; i++)
+#define ALL(v) v.begin(), v.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+
+#define ll int64_t
+
+using P = pair<int, int>;
+
+const int MOD = 1000000007;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    int left = -1, right, mid;
+    rep(i, n) {
+        if (s[i] == '|') {
+            if (left == -1)
+                left = i;
+            else
+                right = i;
+        }
+        if (s[i] == '*') mid = i;
+    }
+    if (left < mid && mid < right)
+        puts("in");
+    else
+        puts("out");
+    return 0;
+}
