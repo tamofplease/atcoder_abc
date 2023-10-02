85bcdc67ee7ed87d92a71b90ad6d7d9e785b09c8
tamofplease <riddimdubstep@yahoo.co.jp>
2023-10-02 23:03:37 +0900

    solver e prob

--- abc304/e/main.cpp
+++ abc304/e/main.cpp
@@ -1,10 +1,15 @@
 // "Copyright 2022 tam"
 #include <bits/stdc++.h>
 
+#include <atcoder/all>
+
 using namespace std;
+using namespace atcoder;
 
 #define rep(i, n) for (int i = 0; i < n; i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
 #define Rep(i, n) for (int i = 1; i <= n; i++)
+#define rRep(i, n) for (int i = n; i >= 1; i--)
 #define ALL(v) v.begin(), v.end()
 #define chmax(x, y) x = max(x, y)
 #define chmin(x, y) x = min(x, y)
@@ -12,26 +17,75 @@ using namespace std;
 #define ll int64_t
 
 using P = pair<int, int>;
+// using mint = modint1000000007;
+using mint = modint998244353;
 
 const int MOD = 1000000007;
+
+template <typename T, int FAC_MAX>
+struct Comb {
+   private:
+    vector<T> fac, ifac;
+
+   public:
+    Comb() {
+        fac.resize(FAC_MAX, 1);
+        ifac.resize(FAC_MAX, 1);
+        Rep(i, FAC_MAX - 1) fac[i] = fac[i - 1] * i;
+        ifac[FAC_MAX - 1] = T(1) / fac[FAC_MAX - 1];
+        rRep(i, FAC_MAX - 2) ifac[i] = ifac[i + 1] * T(i + 1);
+    }
+    T aPb(int a, int b) {
+        if (b < 0 || a < b) return T(0);
+        return fac[a] * ifac[a - b];
+    }
+    T aCb(int a, int b) {
+        if (b < 0 || a < b) return T(0);
+        return fac[a] * ifac[a - b] * ifac[b];
+    }
+    T nHk(int n, int k) {
+        if (n == 0 && k == 0) return T(1);
+        if (n <= 0 || k < 0) return 0;
+        return aCb(n + k - 1, k);
+    }
+    T pairCombination(int n) {
+        if (n % 2 == 1) return T(0);
+        return fac[n] * ifac[n / 2] / (T(2) ^ (n / 2));
+    }
+};
 
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> g;
+    dsu d(n);
     rep(i, m) {
-        int u, v;
-        cin >> u >> v, u--, v--;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        int v, u;
+        cin >> v >> u, u--, v--;
+        d.merge(u, v);
     }
-
+    set<pair<int, int>> bad;
     int k;
     cin >> k;
     rep(i, k) {
-        int x, y, x--, y--;
-        cin >> x >> y;
-        cin >> x[i] >> y[i];
+        int x, y;
+        cin >> x >> y, x--, y--;
+        int xl = d.leader(x), yl = d.leader(y);
+        if (xl < yl) swap(xl, yl);
+        bad.insert({d.leader(xl), d.leader(yl)});
+    }
+
+    int Q;
+    cin >> Q;
+    rep(i, Q) {
+        int p, q;
+        cin >> p >> q, p--, q--;
+        int pl = d.leader(p), ql = d.leader(q);
+        if (pl < ql) swap(pl, ql);
+        if (bad.find({pl, ql}) != bad.end()) {
+            puts("No");
+        } else {
+            puts("Yes");
+        }
     }
 
     return 0;
